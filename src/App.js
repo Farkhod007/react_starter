bb386a98c7c5809f1865879d67da5852c9f4bd15
Developer0123 <mansurxushvaqtov7@gmail.com>
2021-10-31 17:30:09 +0500

    picture elementi yaratildi

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Typography from './components/Typography'
 import './App.css'
+import Picture from './components/Picture'
 
 const App = () => {
 	return (
 		<div>
-			<Typography variant="h1">This is a heading one</Typography>
-			<Typography variant="h2" color="papayawhip">
+			<Typography variant='h1'>This is a heading one</Typography>
+			<Typography variant='h2' color='papayawhip'>
 				This is a heading one
 			</Typography>
-			<Typography variant="bodyOne" color="secondary">
+			<Typography variant='bodyOne' color='secondary'>
 				This is a heading one
 			</Typography>
+			<Picture one='./images/1.jpg' two='./images/2.jpg' three='./images/3.jpg' />
 		</div>
 	)
 }
---
+++ src/components/Picture/styled-components.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+
+const img = styled.img`
+  width: 100%;
+  height: 100vh;
+`
+
+
+const Styled = {
+    img
+}
+
+export default Styled
